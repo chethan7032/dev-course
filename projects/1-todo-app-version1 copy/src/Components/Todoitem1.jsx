b1c66be240e575ed87bab72790bfb6c3713e2c60
chethan7032 <chethanreddy118@gmail.com>
2024-07-14 11:10:36 +0530

    just project names are changed

---
+++ projects/1-todo-app-version1 copy/src/Components/Todoitem1.jsx
@@ -0,0 +1,18 @@
+function Todoiteam1() {
+  let todoname = "Buy milk";
+  let tododate = "4/10/2023";
+  return (
+    <div class="container">
+      <div class="row ch-row">
+        <div class="col-6">{todoname}</div>
+        <div class="col-4">{tododate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger ch-button">
+            Danger
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Todoiteam1;
